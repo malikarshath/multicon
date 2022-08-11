3b5d2a183171bc89180f2f480219736a7465a5c2
malikarshath <malikarshath@gmail.com>
2022-08-11 18:31:24 +0530

    refreshing apex after assigned

--- force-app/main/default/lwc/employeeBrowser/employeeBrowser.js
+++ force-app/main/default/lwc/employeeBrowser/employeeBrowser.js
@@ -1,12 +1,14 @@
-import { LightningElement,wire } from 'lwc';
+import { LightningElement,wire,track } from 'lwc';
 import getEmployees from '@salesforce/apex/EmployeeBrowser.getEmployees';
 import createProjectAssignment from '@salesforce/apex/EmployeeBrowser.createProjectAssignment';
+import { refreshApex } from "@salesforce/apex";
 
 export default class EmployeeBrowser extends LightningElement {
     employee = [];
     selectedProjectSkillId = '';
     selectedProjectId = '';
     selectedEmployeeId = '';
+    @track choose = true;
     
     
     @wire(getEmployees, { projectId: "$selectedProjectId", projectSkillId:"$selectedProjectSkillId"}) employees;
@@ -16,21 +18,31 @@ export default class EmployeeBrowser extends LightningElement {
     handleFilterChange(event) {
         this.selectedProjectSkillId = event.detail.projectSkillId;  
         this.selectedProjectId = event.detail.projectId;
+        if (this.selectedProjectSkillId === '') {
+            this.choose = true;
+        }
     }
     handleClick(){
-        createProjectAssignment({
-            projectId: this.selectedProjectId,
-            projectSkillId: this.selectedProjectSkillId,
-            employeeId: this.selectedEmployeeId
-        }).then(result=>{
-            this.message = 'Project Assigned';
-        }).catch(error=>{
-            this.message = 'Error Assigning project';
-        });
+        if (confirm("Do you want to assign!")) {
+            createProjectAssignment({
+                projectId: this.selectedProjectId,
+                projectSkillId: this.selectedProjectSkillId,
+                employeeId: this.selectedEmployeeId
+            });
+            alert("Project assigned");
+            refreshApex(this.employees);
+        } else {
+            alert("Project not assigned");
+        }
 
     }
     handleEmployeeSelected(event) {
         this.selectedEmployeeId = event.detail.employeeId;
+        if (this.selectedProjectSkillId !== '' && this.selectedEmployeeId !== ''){
+            this.choose = false;
+        } else if (this.selectedProjectSkillId === '' || this.selectedEmployeeId === ''){
+            this.choose = true;
+        }
     }
 
 }
